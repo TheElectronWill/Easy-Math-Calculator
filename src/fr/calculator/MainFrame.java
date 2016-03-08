0aaa1c48a9a6e76db77400d02b876e75ac27dcbd
Guillaume R <TheElectronWill@users.noreply.github.com>
2016-03-08 10:42:10 +0100

    Encodage utf8

--- src/fr/calculator/MainFrame.java
+++ src/fr/calculator/MainFrame.java
@@ -12,7 +12,8 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 /**
- * Fenêtre principale. Utilise la bibliothèque graphique swing.
+ * FenÃªtre principale. Utilise la bibliothÃ¨que graphique swing.
+ * 
  * @author raffin
  */
 public class MainFrame extends JFrame {
@@ -33,43 +34,43 @@ public class MainFrame extends JFrame {
 		c.gridy = 0;
 		c.gridheight = 1;
 		c.gridheight = 1;
-		
+
 		JLabel calcLabel = new JLabel("Calcul:");
-		c.insets = new Insets(15,15,5,5);
+		c.insets = new Insets(15, 15, 5, 5);
 		panel.add(calcLabel, c);
 
 		inputField = new JTextField();
-		c.insets=new Insets(15,0,5,15);
-		c.gridx=1;
+		c.insets = new Insets(15, 0, 5, 15);
+		c.gridx = 1;
 		c.gridwidth = 3;
 		panel.add(inputField, c);
 
 		JButton eraseButton = new JButton("Effacer");
-		c.insets=new Insets(5,0,5,5);
+		c.insets = new Insets(5, 0, 5, 5);
 		c.gridy = 1;
 		c.gridwidth = 1;
 		panel.add(eraseButton, c);
 
-		JButton calculateButton = new JButton("Calculer/Résoudre");
-		c.insets=new Insets(5,5,5,15);
-		c.gridx=2;
-		c.gridwidth=2;
+		JButton calculateButton = new JButton("Calculer/Rï¿½soudre");
+		c.insets = new Insets(5, 5, 5, 15);
+		c.gridx = 2;
+		c.gridwidth = 2;
 		panel.add(calculateButton, c);
 
-		JLabel resultInfos = new JLabel("Résultat:");
-		c.insets=new Insets(15, 15, 15, 5);
-		c.gridx=0;
-		c.gridy=3;
-		c.gridwidth=1;
-		panel.add(resultInfos,c);
-		
-		resultLabel = new JLabel("aucun résultat à afficher pour l'instant");
-		resultLabel.setFont(resultLabel.getFont().deriveFont(Font.ITALIC));
-		c.insets=new Insets(5,0,5,15);
-		c.fill=GridBagConstraints.CENTER;
-		c.gridx=1;
+		JLabel resultInfos = new JLabel("Rï¿½sultat:");
+		c.insets = new Insets(10, 15, 15, 5);
+		c.gridx = 0;
 		c.gridy = 3;
-		c.gridwidth=3;
+		c.gridwidth = 1;
+		panel.add(resultInfos, c);
+
+		resultLabel = new JLabel("aucun rï¿½sultat ï¿½ afficher pour l'instant");
+		resultLabel.setFont(resultLabel.getFont().deriveFont(Font.ITALIC));
+		c.insets = new Insets(0, 0, 5, 15);
+		c.fill = GridBagConstraints.CENTER;
+		c.gridx = 1;
+		c.gridy = 3;
+		c.gridwidth = 3;
 		panel.add(resultLabel, c);
 
 		this.setResizable(false);
