7c355161be685b44b6c797f41351fde71f22e6f4
Guillaume R <TheElectronWill@users.noreply.github.com>
2016-03-08 10:55:54 +0100

    Ajout d'un ActionListener pour chaque bouton.

--- src/fr/calculator/MainFrame.java
+++ src/fr/calculator/MainFrame.java
@@ -4,6 +4,8 @@ import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -46,12 +48,14 @@ public class MainFrame extends JFrame {
 		panel.add(inputField, c);
 
 		JButton eraseButton = new JButton("Effacer");
+		eraseButton.addActionListener(new EraseButtonListener());
 		c.insets = new Insets(5, 0, 5, 5);
 		c.gridy = 1;
 		c.gridwidth = 1;
 		panel.add(eraseButton, c);
 
 		JButton calculateButton = new JButton("Calculer/Résoudre");
+		calculateButton.addActionListener(new CalculateButtonListener());
 		c.insets = new Insets(5, 5, 5, 15);
 		c.gridx = 2;
 		c.gridwidth = 2;
@@ -80,6 +84,45 @@ public class MainFrame extends JFrame {
 
 	public static void main(String[] args) {
 		MainFrame main = new MainFrame();
+	}
+
+	private class EraseButtonListener implements ActionListener {
+		/**
+		 * Appelé quand on clique sur le bouton "Effacer".
+		 */
+		@Override
+		public void actionPerformed(ActionEvent event) {
+			inputField.setText("");// efface le texte
+		}
+	}
+
+	private class CalculateButtonListener implements ActionListener {
+		/**
+		 * Appelé quand on clique sur le bouton "Calculer".
+		 */
+		@Override
+		public void actionPerformed(ActionEvent event) {
+			final JButton calculateButton = (JButton) event.getSource();
+			calculateButton.setText("Calcul en cours...");
+			calculateButton.setEnabled(false);
+			// TODO calcul/résolution de l'équation.
+			// Attention: le calcul doit s'effectuer dans un autre Thread, pour
+			// ne pas bloquer la mise à jour de l'affichage.
+
+			// Exemple de Thread qui attend 1 secondes et on réactive le bouton:
+			Thread t = new Thread() {
+				public void run() {
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					calculateButton.setText("Calculer/Résoudre");
+					calculateButton.setEnabled(true);
+				};
+			};
+			t.start();
+		}
 	}
 
 }
