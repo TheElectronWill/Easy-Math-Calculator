33e83cd8cefbede1bbf15758baa61fae9d262a8c
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-03-09 16:54:14 +0100

    Utilisation de l'EDT et champ de texte agrandi
    Toute modification/création de composant graphique doit se faire dans le thread prévu à cet effet: l'Event Dispatch Thread. Voir le site web d'Oracle: https://docs.oracle.com/javase/tutorial/uiswing/concurrency/index.html
    J'ai modifié la classe principale pour respecter cela.
    J'ai aussi agrandi le champ de texte.

--- src/fr/calculator/MainFrame.java
+++ src/fr/calculator/MainFrame.java
@@ -1,34 +1,58 @@
 package fr.calculator;
 
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 
 /**
  * Fenêtre principale. Utilise la bibliothèque graphique swing.
  * 
- * @author raffin
+ * @author Guillaume
  */
 public class MainFrame extends JFrame {
-
+	
 	private static final long serialVersionUID = 1L;
-	private JTextField inputField;
-	private JLabel resultLabel;
-
+	
+	/**
+	 * Point d'entrée du programme.
+	 */
+	public static void main(String[] args) {
+		// -- Affichage l'interface graphique (dans l'Event Dispatch Thread) --
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				MainFrame frame = new MainFrame();
+				frame.setVisible(true);
+			}
+		});
+	}
+	
+	/**
+	 * Contient le texte entré par l'utilisateur.
+	 */
+	private final JTextField inputField;
+	/**
+	 * Contient le résultat calculé par le logiciel.
+	 */
+	private final JLabel result;
+	
+	/**
+	 * Crée la fenêtre et place les composants graphiques.
+	 */
 	public MainFrame() {
-		this.setTitle("Easy Math Calculator");
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setLocationRelativeTo(null);
-
+		super("Easy Math Calculator");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// termine le programme quand on ferme la fenêtre
+		this.setLocationRelativeTo(null);// centre la fenêtre
+		
 		JPanel panel = new JPanel(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
 		c.fill = GridBagConstraints.HORIZONTAL;
@@ -36,90 +60,72 @@ public class MainFrame extends JFrame {
 		c.gridy = 0;
 		c.gridheight = 1;
 		c.gridheight = 1;
-
+		
 		JLabel calcLabel = new JLabel("Calcul:");
 		c.insets = new Insets(15, 15, 5, 5);
 		panel.add(calcLabel, c);
-
+		
 		inputField = new JTextField();
+		inputField.setPreferredSize(new Dimension(400, 30));
 		c.insets = new Insets(15, 0, 5, 15);
 		c.gridx = 1;
-		c.gridwidth = 3;
+		c.gridwidth = 4;
 		panel.add(inputField, c);
-
+		
 		JButton eraseButton = new JButton("Effacer");
 		eraseButton.addActionListener(new EraseButtonListener());
 		c.insets = new Insets(5, 0, 5, 5);
 		c.gridy = 1;
 		c.gridwidth = 1;
 		panel.add(eraseButton, c);
-
+		
 		JButton calculateButton = new JButton("Calculer/Résoudre");
 		calculateButton.addActionListener(new CalculateButtonListener());
 		c.insets = new Insets(5, 5, 5, 15);
-		c.gridx = 2;
+		c.gridx = 3;
 		c.gridwidth = 2;
 		panel.add(calculateButton, c);
-
-		JLabel resultInfos = new JLabel("Résultat:");
+		
+		JLabel resultLabel = new JLabel("Résultat:");
 		c.insets = new Insets(10, 15, 15, 5);
 		c.gridx = 0;
 		c.gridy = 3;
 		c.gridwidth = 1;
-		panel.add(resultInfos, c);
-
-		resultLabel = new JLabel("aucun résultat à afficher pour l'instant");
-		resultLabel.setFont(resultLabel.getFont().deriveFont(Font.ITALIC));
+		panel.add(resultLabel, c);
+		
+		result = new JLabel("aucun résultat à afficher pour l'instant");
+		result.setFont(result.getFont().deriveFont(Font.ITALIC));
 		c.insets = new Insets(0, 0, 5, 15);
 		c.fill = GridBagConstraints.CENTER;
 		c.gridx = 1;
 		c.gridwidth = 3;
-		panel.add(resultLabel, c);
-
-		this.setResizable(false);
+		panel.add(result, c);
+		
 		this.setContentPane(panel);
-		this.pack();
-		this.setVisible(true);
+		this.setResizable(false);// fenêtre non redimensionnable
+		this.pack();// fait correspondre la taille de la fenêtre à son contenu
 	}
-
-	public static void main(String[] args) {
-		MainFrame main = new MainFrame();
-	}
-
+	
 	private class EraseButtonListener implements ActionListener {
 		/**
-		 * Fonction appelée quand on clique sur le bouton "Effacer".
+		 * Appelée quand on clique sur le bouton "Effacer".
 		 */
 		@Override
 		public void actionPerformed(ActionEvent event) {
 			inputField.setText("");// efface le texte
 		}
 	}
-
+	
 	private class CalculateButtonListener implements ActionListener {
 		/**
-		 * Fonction appelée quand on clique sur le bouton "Calculer".
+		 * Appelée quand on clique sur le bouton "Calculer".
 		 */
 		@Override
 		public void actionPerformed(ActionEvent event) {
 			final JButton calculateButton = (JButton) event.getSource();
 			calculateButton.setText("Calcul en cours...");
 			calculateButton.setEnabled(false);
-
-			// Le calcul doit s'effectuer dans un autre Thread, pour
-			// ne pas bloquer la mise à jour de l'affichage.
-			CalculatorThread calculator = new CalculatorThread();
-			calculator.start();// lance le calcul en parallèle
-
-			/*
-			 * Exemple de Thread qui attend 1 seconde et on réactive le bouton:
-			 * Thread t = new Thread() { public void run() { try {
-			 * Thread.sleep(1000); } catch (InterruptedException e) {
-			 * e.printStackTrace(); }
-			 * calculateButton.setText("Calculer/Résoudre");
-			 * calculateButton.setEnabled(true); }; }; t.start();
-			 */
 		}
 	}
-
+	
 }
