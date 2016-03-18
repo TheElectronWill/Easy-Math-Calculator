7122c17d36f9ce65b8e65a2781227b894d82263f
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-03-18 17:55:33 +0100

    Amélioration du MathParser

--- src/fr/calculator/parser/Main.java
+++ src/fr/calculator/parser/Main.java
@@ -5,19 +5,20 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-
+	
 	public static void main(String[] args) throws InterruptedException {
 		System.out.println("Entrez une expression mathématique");
 		Scanner sc = new Scanner(System.in);
 		String expression = sc.nextLine();
 		sc.close();
-
+		
 		long t0 = System.nanoTime();
 		List<Term> terms = new MathParser(expression).parse();
-		long time = System.nanoTime() - t0;
-		double seconds = time / Math.pow(10, 9);
-		System.out.println("Analysé en " + time + " nanosecondes, soit " + seconds + " secondes");
+		long nanos = System.nanoTime() - t0;
+		double millis = nanos / Math.pow(10, 6);
+		double seconds = millis / 1000.0;
+		System.out.println("Analysé en " + seconds + " secondes  (" + nanos + " ns = " + millis + " ms )");
 		System.out.println(Arrays.deepToString(terms.toArray()));
 	}
-
+	
 }
