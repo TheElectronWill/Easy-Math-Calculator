2d560baf07f27bf3966139964127052f25742f35
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-03-20 00:05:30 +0100

    Test modifié

--- src/fr/calculator/solver/TestSimplifier.java
+++ src/fr/calculator/solver/TestSimplifier.java
@@ -8,11 +8,10 @@ public class TestSimplifier {
 	
 	public static void main(String[] args) throws InterruptedException {
 		IntegerTerm i1 = new IntegerTerm(10), i2 = new IntegerTerm(5);
-		Literal x = new Literal("x");
-		Multiplication m = new Multiplication(x, new Multiplication(i2, new Division(i1, new Fraction(i2.value, i1.value))));
+		Multiplication m = new Multiplication(new Literal("x"), new Multiplication(i2, new Division(i1, new Fraction(i2.value, i1.value))));
 		
-		List<Term> terms = Arrays.asList(new IntegerTerm(99), new Parenthesis(m),
-				new Parenthesis(new IntegerTerm(123), new Fraction(12, 10)));
+		List<Term> terms = Arrays.asList(new IntegerTerm(-20), new Parenthesis(m),
+				new Parenthesis(new IntegerTerm(2), new Fraction(25, 10)));
 		String beforeSimplification = Arrays.deepToString(terms.toArray());
 		System.out.println("tel quel : " + beforeSimplification);
 		
