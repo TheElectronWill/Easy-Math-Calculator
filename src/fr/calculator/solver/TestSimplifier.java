16b6af31e62f811a37564478a3a48c416fd77865
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-03-19 21:01:49 +0100

    Mesure du temps lors d'un test

--- src/fr/calculator/solver/TestSimplifier.java
+++ src/fr/calculator/solver/TestSimplifier.java
@@ -11,7 +11,7 @@ import fr.calculator.parser.Term;
 
 public class TestSimplifier {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		IntegerTerm i1 = new IntegerTerm(10), i2 = new IntegerTerm(5);
 		Literal x = new Literal("x");
 		Multiplication m = new Multiplication(x, new Multiplication(i2, new Division(i1, new Fraction(i2.value, i1.value))));
@@ -20,9 +20,12 @@ public class TestSimplifier {
 		String beforeSimplification = Arrays.deepToString(terms.toArray());
 		System.out.println("tel quel : " + beforeSimplification);
 		
+		long t0 = System.nanoTime();
 		List<Term> simplified = MathSimplifier.simplify(terms);
+		double time = (System.nanoTime() - t0) / (1000_000.0);
 		String afterSimplification = Arrays.deepToString(simplified.toArray());
 		System.out.println("simplifié: " + afterSimplification);
+		System.out.println("Simplifié en " + time + " millisecondes");
 	}
 	
 }
