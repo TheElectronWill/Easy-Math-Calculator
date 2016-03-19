16ed982bf254c1692b09048ccc7ca43a0202d7c7
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-03-19 17:00:12 +0100

    Ajout d'une classe pour représenter les valeurs litérales telles que "x" ou "pi"

---
+++ src/fr/calculator/parser/Literal.java
@@ -0,0 +1,31 @@
+package fr.calculator.parser;
+
+public class Literal implements Term {
+	
+	public final String name;
+	
+	public Literal(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public Term reverse() {
+		return new Division(new IntegerTerm(1), this);
+	}
+	
+	@Override
+	public Term negate() {
+		return new Multiplication(new IntegerTerm(-1), this);
+	}
+	
+	@Override
+	public Term simplify() {
+		return this;
+	}
+	
+	@Override
+	public String toString() {
+		return "Literal: " + name;
+	}
+	
+}
