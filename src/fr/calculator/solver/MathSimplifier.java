fb2c7e999fccffc7baaaa1f0dab8020e66e2164e
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-03-19 23:32:09 +0100

    Regroupe les constantes

--- src/fr/calculator/solver/MathSimplifier.java
+++ src/fr/calculator/solver/MathSimplifier.java
@@ -1,7 +1,11 @@
 package fr.calculator.solver;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import fr.calculator.parser.Fraction;
+import fr.calculator.parser.IntegerTerm;
+import fr.calculator.parser.Parenthesis;
 import fr.calculator.parser.Term;
 
 /**
@@ -14,20 +18,36 @@ public class MathSimplifier {
 	
 	private MathSimplifier() {}
 	
-	public static List<Term> simplify(Term[] expression) {
-		List<Term> simplifiedExpression = new ArrayList<>();
-		for (Term t : expression) {
-			simplifiedExpression.add(t.simplify());
-		}
-		return simplifiedExpression;
+	public static List<Term> simplify(Term... terms) {
+		return simplify(Arrays.asList(terms));
 	}
 	
-	public static List<Term> simplify(List<Term> expression) {
-		List<Term> simplifiedExpression = new ArrayList<>();
-		for (Term t : expression) {
-			simplifiedExpression.add(t.simplify());
+	public static List<Term> simplify(List<Term> terms) {
+		Fraction constant = new Fraction(0, 1);// somme de toutes les constantes
+		List<Term> simplifiedTerms = new ArrayList<>();
+		for (Term t : terms) {
+			t = t.simplify();
+			if (t instanceof Parenthesis) {// parenthèse seule -> on développe
+				for (Term pt : ((Parenthesis) t).terms) {
+					pt = pt.simplify();
+					if (pt instanceof IntegerTerm) {
+						constant.add((IntegerTerm) t);
+					} else if (pt instanceof Fraction) {
+						constant.add((Fraction) t);
+					} else {
+						simplifiedTerms.add(pt);
+					}
+				}
+			} else if (t instanceof IntegerTerm) {
+				constant.add((IntegerTerm) t);
+			} else if (t instanceof Fraction) {
+				constant.add((Fraction) t);
+			} else {
+				simplifiedTerms.add(t);
+			}
 		}
-		return simplifiedExpression;
+		simplifiedTerms.add(constant.simplify());
+		return simplifiedTerms;
 	}
 	
 }
