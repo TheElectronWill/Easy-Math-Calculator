730acb373b2ac6a39d287ed5f2e550d0ac38d2bd
SYLVAINGEISER <210.geiser.sylvain1@gmail.com>
2016-04-05 09:50:07 +0200

    Prise en charge des fractions pour les fonctions affines

--- MathSolver.java
+++ MathSolver.java
@@ -1,13 +1,17 @@
 package fr.calculator.solver;
 import java.util.List;
-import fr.calculator.SimplificationFraction;
-import fr.calculator.parser.*;
+
+import fr.calculator.Exponentielle;
+import fr.calculator.parser.Fraction;
+import fr.calculator.parser.IntegerTerm;
+import fr.calculator.parser.Multiplication;
+import fr.calculator.parser.Term;
 public class MathSolver {
 	public static String solver (List<Term> gauche, List<Term> droit) {
-			int fact = 0, fact1 = 0, cons1 = 0, fact2 = 0, cons2 = 0, valeur = 0, compt = 0, num1 = 0, denom1 = 0;
+			int fact1 = 0, fact11 = 0, fact111 = 0, fact2 = 0, fact22 = 0, fact222 = 0, cons1 = 0, cons11 = 0, cons111 = 0, cons2 = 0, cons22 = 0, cons222 = 0, valeur = 0, compt = 0, num1 = 0, denom1 = 0;
 			double solution = 0;
 			if (gauche == droit) {
-				System.out.print("L'égalité est toujours vraie.");
+				System.out.print("L'Ã©galitÃ© est toujours vraie");
 			}
 			Term X = gauche.get(0);
 			Term X2 = droit.get(0);
@@ -20,131 +24,140 @@ public class MathSolver {
 				Exponentielle e = (Exponentielle) X, e2 = (Exponentielle) X2;
 				Term a = e.exp, b = e2.exp;
 				if (a instanceof IntegerTerm) {
-					solution = a;
+					solution = ((IntegerTerm)a).value;
 				}
 				else {
-					solution = b;
+					solution = ((IntegerTerm)b).value;
 				}
 			}
 			else if (X instanceof Exponentielle) {
 				Exponentielle e = (Exponentielle) X;
 				Term a = e.exp;
 				if (a instanceof IntegerTerm) {
-					solution = Math.exp(a);
+					solution = Math.exp(((IntegerTerm)a).value);
 				}
 				else {
-					solution = Math.log(b);
+					solution = Math.log(((IntegerTerm)X2).value);
 				}
 			}
 			else if (X2 instanceof Exponentielle) {
 				Exponentielle e2 = (Exponentielle) X2;
 				Term b = e2.exp;
-				if (a instanceof IntegerTerm) {
-					solution = Math.log(a);
+				if (X instanceof IntegerTerm) {
+					solution = Math.log(((IntegerTerm)b).value);
 				}
 				else {
-					solution = Math.exp(b);
+					solution = Math.exp(((IntegerTerm)X).value);
 				}
 			}
 			if (X instanceof Logarithme && X2 instanceof Logarithme) {
 				Logarithme l = (Logarithme) X, l2 = (Logarithme) X2;
 				Term a = l.log, b = l2.log;
 				if (a instanceof IntegerTerm) {
-					solution = a;
+					solution = ((IntegerTerm)a).value;
 				}
 				else {
-					solution = b;
+					solution = ((IntegerTerm)b).value;
 				}
 			}
 			else if (X instanceof Logarithme) {
 				Logarithme l = (Logarithme) X;
 				Term a = l.log;
 				if (a instanceof IntegerTerm) {
-					solution = Math.log(a);
+					solution = Math.log(((IntegerTerm)a).value);
 				}
 				else {
-					solution = Math.exp(b);
+					solution = Math.exp(((IntegerTerm)X2).value);
 				}
 			}
 			else if (X2 instanceof Logarithme) {
 				Logarithme l2 = (Logarithme) X2;
 				Term b = l2.log;
-				if (a instanceof IntegerTerm) {
-					solution = Math.exp(a);
+				if (X instanceof IntegerTerm) {
+					solution = Math.exp(((IntegerTerm)X).value);
 				}
 				else {
-					solution = Math.log(b);
+					solution = Math.log(((IntegerTerm)b).value);
 				}
 			}
 			if (X instanceof Cosinus && X2 instanceof Cosinus) {
 				Cosinus c = (Cosinus) X, c2 = (Cosinus) X2;
 				Term a = c.cos, b = c2.cos;
 				if (a instanceof IntegerTerm) {
-					solution = a;
+					solution = ((IntegerTerm)a).value;
 				}
 				else {
-					solution = b;
+					solution = ((IntegerTerm)b).value;
 				}
 			}
 			else if (X instanceof Cosinus) {
 				Cosinus c = (Cosinus) X;
 				Term a = c.cos;
 				if (a instanceof IntegerTerm) {
-					solution = Math.cos(a);
+					solution = Math.cos(((IntegerTerm)a).value);
 				}
 				else {
-					solution = Math.arccos(b);
+					solution = Math.acos(((IntegerTerm)X2).value);
 				}
 			}
 			else if (X2 instanceof Cosinus) {
 				Cosinus c2 = (Cosinus) X2;
 				Term b = c2.cos;
-				if (a instanceof IntegerTerm) {
-					solution = Math.arccos(a);
+				if (X instanceof IntegerTerm) {
+					solution = Math.acos(((IntegerTerm)X).value);
 				}
 				else {
-					solution = Math.cos(b);
+					solution = Math.cos(((IntegerTerm)b).value);
 				}
 			}
 			if (X instanceof Sinus && X2 instanceof Sinus) {
 				Sinus s = (Sinus) X, s2 = (Sinus) X2;
 				Term a = s.sin, b = s2.sin;
 				if (a instanceof IntegerTerm) {
-					solution = a;
+					solution = ((IntegerTerm)a).value;
 				}
 				else {
-					solution = b;
+					solution = ((IntegerTerm)b).value;
 				}
 			}
 			else if (X instanceof Sinus) {
 				Sinus s = (Sinus) X;
 				Term a = s.sin;
 				if (a instanceof IntegerTerm) {
-					solution = Math.sin(a);
+					solution = Math.sin(((IntegerTerm)a).value);
 				}
 				else {
-					solution = Math.arcsin(b);
+					solution = Math.asin(((IntegerTerm)X2).value);
 				}
 			}
 			else if (X2 instanceof Sinus) {
 				Sinus s2 = (Sinus) X2;
 				Term b = s2.sin;
-				if (a instanceof IntegerTerm) {
-					solution = Math.arcsin(a);
+				if (X instanceof IntegerTerm) {
+					solution = Math.asin(((IntegerTerm)X).value);
 				}
 				else {
-					solution = Math.sin(b);
+					solution = Math.sin(((IntegerTerm)b).value);
 				}
 			}		
 			else {
+				int num = 0, denom = 0;
 				if (X instanceof Multiplication) {
 					Multiplication m = (Multiplication) X;
 					Term a = m.a, b = m.b;
 					if (a instanceof IntegerTerm) {
 						fact1 = ((IntegerTerm)a).value;				
 					}
-					else {
-						fact1 = ((IntegerTerm)b).value;			 
+					else if (a instanceof Fraction) {
+						fact11 = ((Fraction)a).numerator;
+						fact111 = ((Fraction)a).denominator;
+					}
+					if (b instanceof IntegerTerm) {
+						fact1 = ((IntegerTerm)b).value;				
+					}
+					else if (b instanceof Fraction) {
+						fact11 = ((Fraction)b).numerator;
+						fact111 = ((Fraction)b).denominator;
 					}
 				}
 				else {
@@ -156,8 +169,16 @@ public class MathSolver {
 					if (a instanceof IntegerTerm) {
 						fact2 = ((IntegerTerm)a).value;				
 					}
-					else {
-						fact2 = ((IntegerTerm)b).value;			 
+					else if (a instanceof Fraction ){
+						fact22 = ((Fraction)a).numerator;
+						fact222 = ((Fraction)a).denominator;
+					}
+					if (b instanceof IntegerTerm) {
+						fact2 = ((IntegerTerm)b).value;				
+					}
+					else if (b instanceof Fraction ){
+						fact22 = ((Fraction)b).numerator;
+						fact222 = ((Fraction)b).denominator;
 					}
 				}
 				else {
@@ -169,8 +190,16 @@ public class MathSolver {
 					if (a instanceof IntegerTerm) {
 						fact1 = ((IntegerTerm)a).value;				
 					}
-					else {
-						fact1 = ((IntegerTerm)b).value;			 
+					else if (a instanceof Fraction ){
+						fact11 = ((Fraction)a).numerator;
+						fact111 = ((Fraction)a).denominator;
+					}
+					if (b instanceof IntegerTerm) {
+						fact1 = ((IntegerTerm)b).value;				
+					}
+					else if (b instanceof Fraction ){
+						fact11 = ((Fraction)b).numerator;
+						fact111 = ((Fraction)b).denominator;
 					}
 				}
 				else if (Y instanceof IntegerTerm){
@@ -178,15 +207,22 @@ public class MathSolver {
 				}
 				else {
 					cons1 = 0;
-				}
 				if (Y2 instanceof Multiplication) {
 					Multiplication m4 = (Multiplication) Y2;
 					Term a = m4.a, b = m4.b;
 					if (a instanceof IntegerTerm) {
 						fact2 = ((IntegerTerm)a).value;				
 					}
-					else {
-						fact2 = ((IntegerTerm)b).value;			 
+					else if (a instanceof Fraction ){
+						fact22 = ((Fraction)a).numerator;
+						fact222 = ((Fraction)a).denominator;
+					}
+					if (b instanceof IntegerTerm) {
+						fact2 = ((IntegerTerm)b).value;				
+					}
+					else if (b instanceof Fraction ){
+						fact22 = ((Fraction)b).numerator;
+						fact222 = ((Fraction)b).denominator;
 					}
 				}
 				else if (Y2 instanceof IntegerTerm){
@@ -195,18 +231,35 @@ public class MathSolver {
 				else {
 					cons2 = 0;
 				}
-				int denom = fact1 - fact2, num = cons1 - cons2;
-				Fraction frac = new Fraction(num, denom);
-				Term simplifié = frac.simplify();
-				if (simplifié instanceof IntegerTerm) {
-					String solution = String.valueOf(((IntegerTerm)simplifié).value);
+				if (fact1 != 0 && fact2 != 0) {
+					denom = fact1 - fact2;
+				}
+				else if (fact1 != 0){
+					denom = fact1*fact222 - fact22;
 				}
 				else {
-					String num1 = String.valueOf(((IntegerTerm)simplifié.numerator).value);
-					String denom1 = String.valueOf(((IntegerTerm)simplifié.denominator).value);
+					denom = fact2*fact111 - fact11;
+				}
+				if (cons1 != 0 && cons2 != 0) {
+					num = cons1 - cons2;
+				}
+				else if (fact1 != 0){
+					num = cons1*cons222 - cons22;
+				}
+				else {
+					num = cons2*cons111 - cons11;
+				}
+				Fraction frac = new Fraction(num, denom);
+				Term simplifiÃ© = frac.simplify();
+				if (simplifiÃ© instanceof IntegerTerm) {
+					solution = (((IntegerTerm)simplifiÃ©).value);
+				}
+				else {
+					num1 = ((Fraction)simplifiÃ©).numerator;
+					denom1 = ((Fraction)simplifiÃ©).denominator;
 				}
 			}
-			System.out.print("La solution à cette équation est: ");
+			System.out.print("La solution Ã  cette Ã©quation est: ");
 			if (num1 == 0 && denom1 == 0) {	
 				System.out.print(solution);
 			}
@@ -216,5 +269,6 @@ public class MathSolver {
 				System.out.print(denom1);
 			}
 			System.out.print(".");
+		}
 	}
 }
