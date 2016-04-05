ffda51033c4eded6e4b3d9ebef5e4e44c632fe01
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-04-05 09:40:51 +0200

    Ajout d'une classe Fonction
    Pour représenter les fonctions telles que cosinus, sinus, exponentielle, logarithme, etc.

---
+++ src/fr/calculator/parser/Fonction.java
@@ -0,0 +1,29 @@
+package fr.calculator.parser;
+
+public class Fonction implements Term {
+
+	/**
+	 * Le nom de la fonction, par exemple "cosinus".
+	 */
+	public String nom;
+	/**
+	 * Ce qui est passé en paramètre de la fonction. Par exemple, dans "cosinus(2x+3)", t vaut Parenthese(2*x, 3).
+	 */
+	public Term t;
+
+	@Override
+	public Term inverser() {
+		return new Division(new NombreEntier(1), this);
+	}
+
+	@Override
+	public Term negatif() {
+		return new Multiplication(new NombreEntier(-1), this);
+	}
+
+	@Override
+	public Term simplifier() {
+		return this;
+	}
+
+}
