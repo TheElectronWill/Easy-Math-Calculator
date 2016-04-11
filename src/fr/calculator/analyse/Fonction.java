342e1b3bbbb0dbcc04e3f0bb6265d19bf93811d9
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-04-11 15:50:18 +0200

    Utilisation d'un enum pour le nom de la fonction

--- src/fr/calculator/analyse/Fonction.java
+++ src/fr/calculator/analyse/Fonction.java
@@ -1,34 +1,38 @@
 package fr.calculator.analyse;
 
 public class Fonction implements Term {
-
+	
 	/**
-	 * Le nom de la fonction, par exemple "cosinus".
+	 * Le nom de la fonction, par exemple COSINUS.
 	 */
-	public String nom;
+	public NomFonction nom;
 	/**
 	 * Ce qui est passé en paramètre de la fonction. Par exemple, dans "cosinus(2x+3)", t vaut Parenthese(2*x, 3).
 	 */
 	public Term t;
-
+	
 	@Override
 	public Term inverser() {
 		return new Division(new NombreEntier(1), this);
 	}
-
+	
 	@Override
 	public Term negatif() {
 		return new Multiplication(new NombreEntier(-1), this);
 	}
-
+	
 	@Override
 	public Term simplifier() {
 		return this;
 	}
-
+	
 	@Override
 	public String toString() {
 		return nom + "(" + t + ")";
 	}
-
+	
+	public enum NomFonction {
+		EXPONENTIELLE, LOGARITHME_NEPERIEN, COSINUS, SINUS;
+	}
+	
 }
