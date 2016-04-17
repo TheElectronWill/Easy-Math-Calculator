f5b292bb7aad3e0855a93b5317c9d4c3b6f4a28e
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-04-17 14:37:30 +0200

    Gestion des erreurs
    Une erreur durant le calcul ou la résolution affiche une fenêtre avec le message d'erreur.

--- src/fr/calculator/Calculateur.java
+++ src/fr/calculator/Calculateur.java
@@ -2,18 +2,16 @@ package fr.calculator;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.swing.JOptionPane;
 import javax.swing.SwingWorker;
-
 import fr.calculator.analyse.MathAnalyseur;
 import fr.calculator.analyse.Term;
 import fr.calculator.resolution.MathSimplifieur;
 import fr.calculator.resolution.MathSolveur;
 
 /**
- * Un <code>SwingWorker</code> qui effectue les calculs dans un autre Thread que l'EDT (Event Dispatch Thread, le thread dans lequel doit
- * s'effectuer toute modification de composant graphique).
+ * Un <code>SwingWorker</code> qui effectue les calculs dans un autre Thread que l'EDT (Event Dispatch Thread, le thread
+ * dans lequel doit s'effectuer toute modification de composant graphique).
  * <p>
  * Un nouveau <code>Calculator</code> est créé à chaque fois qu'on clique sur le bouton "Calculer".
  * </p>
@@ -21,38 +19,45 @@ import fr.calculator.resolution.MathSolveur;
  * @author Guillaume
  */
 public class Calculateur extends SwingWorker<String, Void> {
-
+	
 	/**
-	 * La fenêtre principale. Cette référence est nécessaire pour accéder aux composants graphiques qui sont affectés par le calcul, comme
-	 * le JLabel contenant le résultat.
+	 * La fenêtre principale. Cette référence est nécessaire pour accéder aux composants graphiques qui sont affectés
+	 * par le calcul, comme le JLabel contenant le résultat.
 	 */
 	private final Fenetre fenetre;
-
+	
 	public Calculateur(Fenetre fenetre) {
 		this.fenetre = fenetre;
 	}
-
+	
 	/**
 	 * Effectue les calculs et donne le résultat sous forme de chaîne de caractères.
 	 */
 	@Override
 	protected String doInBackground() throws Exception {
-		final String calcul = fenetre.calcul.getText();// le calcul entré par l'utilisateur
-		final String[] parties = calcul.split("=");
-
-		if (parties.length > 2)
-			throw new IllegalArgumentException("Equation invalide: vous ne devez pas entrer plus d'un signe égal");
-
-		final List<List<Term>> termesParties = new ArrayList<>(parties.length);// termes simplifiés
-		for (String partie : parties) {
-			MathAnalyseur analyseur = new MathAnalyseur(partie);
-			List<Term> termes = MathSimplifieur.simplifier(analyseur.analyser());
-			termesParties.add(termes);
+		try {
+			final String calcul = fenetre.calcul.getText();// le calcul entré par l'utilisateur
+			final String[] parties = calcul.split("=");
+			
+			if (parties.length > 2)
+				throw new IllegalArgumentException("Equation invalide: vous ne devez pas entrer plus d'un signe égal");
+				
+			final List<List<Term>> termesParties = new ArrayList<>(parties.length);// termes simplifiés
+			for (String partie : parties) {
+				MathAnalyseur analyseur = new MathAnalyseur(partie);
+				List<Term> termes = MathSimplifieur.simplifier(analyseur.analyser());
+				termesParties.add(termes);
+			}
+			
+			return (parties.length == 1) ? termesParties.get(0).toString()
+					: MathSolveur.resoudre(termesParties.get(0), termesParties.get(1));
+		} catch (Exception e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, e.toString(), "Erreur", JOptionPane.ERROR_MESSAGE);
+			return "ERREUR";
 		}
-
-		return (parties.length == 1) ? termesParties.get(0).toString() : MathSolveur.resoudre(termesParties.get(0), termesParties.get(1));
 	}
-
+	
 	/**
 	 * Affiche le résultat calculé. Cette méthode est appelée dans l'EDT.
 	 */
@@ -68,7 +73,7 @@ public class Calculateur extends SwingWorker<String, Void> {
 																									// d'erreur dans une
 																									// nouvelle fenêtre.
 		}
-
+		
 	}
-
+	
 }
