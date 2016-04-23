1e4079ae94e4ae0889adc553f7d2416ba8fde11b
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-04-23 19:46:51 +0200

    Utilisation d'ExpressionSimples

--- src/fr/calculator/Calculateur.java
+++ src/fr/calculator/Calculateur.java
@@ -1,35 +1,33 @@
 package fr.calculator;
 
-import java.util.ArrayList;
-import java.util.List;
-import javax.swing.JOptionPane;
-import javax.swing.SwingWorker;
 import fr.calculator.analyse.MathAnalyseur;
+import fr.calculator.resolution.ExpressionSimple;
 import fr.calculator.resolution.MathSimplifieur;
 import fr.calculator.resolution.MathSolveur;
-import fr.calculator.analyse.Terme;
+import javax.swing.JOptionPane;
+import javax.swing.SwingWorker;
 
 /**
- * Un <code>SwingWorker</code> qui effectue les calculs dans un autre Thread que l'EDT (Event Dispatch Thread, le thread
- * dans lequel doit s'effectuer toute modification de composant graphique).
+ * Un <code>SwingWorker</code> qui effectue les calculs dans un autre Thread que l'EDT (Event Dispatch Thread,
+ * le thread dans lequel doit s'effectuer toute modification de composant graphique).
  * <p>
  * Un nouveau <code>Calculator</code> est créé à chaque fois qu'on clique sur le bouton "Calculer".
  * </p>
- * 
+ *
  * @author Guillaume
  */
 public class Calculateur extends SwingWorker<String, Void> {
-	
+
 	/**
-	 * La fenêtre principale. Cette référence est nécessaire pour accéder aux composants graphiques qui sont affectés
-	 * par le calcul, comme le JLabel contenant le résultat.
+	 * La fenêtre principale. Cette référence est nécessaire pour accéder aux composants graphiques qui sont
+	 * affectés par le calcul, comme le JLabel contenant le résultat.
 	 */
 	private final Fenetre fenetre;
-	
+
 	public Calculateur(Fenetre fenetre) {
 		this.fenetre = fenetre;
 	}
-	
+
 	/**
 	 * Effectue les calculs et donne le résultat sous forme de chaîne de caractères.
 	 */
@@ -38,26 +36,26 @@ public class Calculateur extends SwingWorker<String, Void> {
 		try {
 			final String calcul = fenetre.calcul.getText();// le calcul entré par l'utilisateur
 			final String[] parties = calcul.split("=");
-			
-			if (parties.length > 2)
+
+			if (parties.length > 2) {
 				throw new IllegalArgumentException("Equation invalide: vous ne devez pas entrer plus d'un signe égal");
-				
-			final List<List<Terme>> termesParties = new ArrayList<>(parties.length);// termes simplifiés
-			for (String partie : parties) {
-				MathAnalyseur analyseur = new MathAnalyseur(partie);
-				List<Terme> termes = MathSimplifieur.simplifier(analyseur.analyser());
-				termesParties.add(termes);
 			}
-			
-			return (parties.length == 1) ? termesParties.get(0).toString()
-					: MathSolveur.resoudre(termesParties.get(0), termesParties.get(1));
+
+			final ExpressionSimple[] expressionsSimples = new ExpressionSimple[parties.length];
+			for (int i = 0; i < parties.length; i++) {
+				MathAnalyseur analyseur = new MathAnalyseur(parties[i]);
+				expressionsSimples[i] = MathSimplifieur.simplifierExpression(analyseur.analyser());
+			}
+
+			return (parties.length == 1) ? expressionsSimples[0].toMathString()
+					: MathSolveur.resoudre(expressionsSimples[0].getTermes(), expressionsSimples[1].getTermes());
 		} catch (Exception e) {
 			e.printStackTrace();
 			JOptionPane.showMessageDialog(null, e.toString(), "Erreur", JOptionPane.ERROR_MESSAGE);
 			return "ERREUR";
 		}
 	}
-	
+
 	/**
 	 * Affiche le résultat calculé. Cette méthode est appelée dans l'EDT.
 	 */
@@ -69,11 +67,9 @@ public class Calculateur extends SwingWorker<String, Void> {
 			fenetre.boutonCalculer.setText("Calculer/Résoudre");
 		} catch (Exception e) {
 			e.printStackTrace();// écrit les détails de l'erreur dans la console.
-			JOptionPane.showMessageDialog(null, e.toString(), "Erreur", JOptionPane.ERROR_MESSAGE);// affiche un message
-																									// d'erreur dans une
-																									// nouvelle fenêtre.
+			JOptionPane.showMessageDialog(null, e.toString(), "Erreur", JOptionPane.ERROR_MESSAGE);// affiche un message d'erreur dans une nouvelle fenêtre.
 		}
-		
+
 	}
-	
+
 }
