95064496432a0a342e013348fe1719b4d3989b53
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-04-26 10:11:59 +0200

    Gestion des problèmes
    Détecte si l'utilsateur à entré une équation sans variable, et affiche une erreur.
    Traite les erreurs de type MathException et ArithmeticException à part pour ne pas aficher la classe de l'erreur dans la fenêtre, mais uniquement le message d'erreur en français.

--- src/fr/calculator/Calculateur.java
+++ src/fr/calculator/Calculateur.java
@@ -8,10 +8,12 @@ import javax.swing.JOptionPane;
 import javax.swing.SwingWorker;
 
 /**
- * Un <code>SwingWorker</code> qui effectue les calculs dans un autre Thread que l'EDT (Event Dispatch Thread,
- * le thread dans lequel doit s'effectuer toute modification de composant graphique).
+ * Un <code>SwingWorker</code> qui effectue les calculs dans un autre Thread que
+ * l'EDT (Event Dispatch Thread, le thread dans lequel doit s'effectuer toute
+ * modification de composant graphique).
  * <p>
- * Un nouveau <code>Calculator</code> est créé à chaque fois qu'on clique sur le bouton "Calculer".
+ * Un nouveau <code>Calculator</code> est créé à chaque fois qu'on clique sur le
+ * bouton "Calculer".
  * </p>
  *
  * @author Guillaume
@@ -19,8 +21,9 @@ import javax.swing.SwingWorker;
 public class Calculateur extends SwingWorker<String, Void> {
 
 	/**
-	 * La fenêtre principale. Cette référence est nécessaire pour accéder aux composants graphiques qui sont
-	 * affectés par le calcul, comme le JLabel contenant le résultat.
+	 * La fenêtre principale. Cette référence est nécessaire pour accéder aux
+	 * composants graphiques qui sont affectés par le calcul, comme le JLabel
+	 * contenant le résultat.
 	 */
 	private final Fenetre fenetre;
 
@@ -29,16 +32,19 @@ public class Calculateur extends SwingWorker<String, Void> {
 	}
 
 	/**
-	 * Effectue les calculs et donne le résultat sous forme de chaîne de caractères.
+	 * Effectue les calculs et donne le résultat sous forme de chaîne de
+	 * caractères.
 	 */
 	@Override
 	protected String doInBackground() throws Exception {
 		try {
 			final String calcul = fenetre.calcul.getText();// le calcul entré par l'utilisateur
 			final String[] parties = calcul.split("=");
-
 			if (parties.length > 2) {
-				throw new IllegalArgumentException("Equation invalide: vous ne devez pas entrer plus d'un signe égal");
+				throw new MathException("Equation invalide : vous ne devez pas entrer plus d'un signe égal");
+			}
+			if (parties.length == 2 && (!parties[0].contains("x") || !parties[1].contains("x"))) {
+				throw new MathException("Equation invalide : vous devez utiliser la variable x");
 			}
 
 			final ExpressionSimple[] expressionsSimples = new ExpressionSimple[parties.length];
@@ -49,6 +55,11 @@ public class Calculateur extends SwingWorker<String, Void> {
 
 			return (parties.length == 1) ? expressionsSimples[0].toMathString()
 					: MathSolveur.resoudre(expressionsSimples[0].getTermes(), expressionsSimples[1].getTermes());
+		} catch (MathException | ArithmeticException e) {
+			String msg = e.getLocalizedMessage().equals("/ by zero") ? "Impossible de diviser par zéro" : e.getLocalizedMessage();
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, msg, "Erreur", JOptionPane.ERROR_MESSAGE);
+			return "ERREUR";
 		} catch (Exception e) {
 			e.printStackTrace();
 			JOptionPane.showMessageDialog(null, e.toString(), "Erreur", JOptionPane.ERROR_MESSAGE);
