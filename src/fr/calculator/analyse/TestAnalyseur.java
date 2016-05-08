a47222e4a9bb7f81977973dcefe46ed17cc3ef6f
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-05-08 22:10:42 +0200

    Améliorations des test
    Basés sur l'entrée de l'utilisateur, ou sur une expression par défaut.

--- src/fr/calculator/analyse/TestAnalyseur.java
+++ src/fr/calculator/analyse/TestAnalyseur.java
@@ -13,26 +13,20 @@ import java.util.Scanner;
 public class TestAnalyseur {
 
 	public static void main(String[] args) throws InterruptedException {
-		Rationnel r = new Rationnel(1, 1);
-		System.out.println(r);
-		System.out.println(r.soustraire(new Rationnel(0, 1)));
-		System.out.println(new Rationnel(1, 1).ajouter(new Rationnel(0, 1)));
-		System.out.println("Entrez une expression mathématique, ou appuyez sur entrée pour l'expression de test par défaut.");
+		System.out.println("Entrez une expression mathématique, ou appuyez sur entrée utiliser l'expression de test par défaut.");
 		Scanner sc = new Scanner(System.in);
 		String expression = sc.nextLine();
 		sc.close();
-
 		if (expression.isEmpty()) {
 			expression = "-22x + -24x^2 - +45 / 2 (56+4) - x(2+4)(3x-1) - xxxcos(2)ln(4)xsin(x) + 2 / 2*pi";
 			System.out.println("L'expression par défaut va être utilisée : " + expression);
 		}
 		long t0 = System.nanoTime();
-		List<Terme> terms = new MathAnalyseur(expression).analyser();
+		List<Terme> termes = new MathAnalyseur(expression).analyser();
 		long nanos = System.nanoTime() - t0;
 		double millis = nanos / Math.pow(10, 6);
-		double secondes = millis / 1000.0;
-		System.out.println("Analysé en " + secondes + " secondes  (" + nanos + " ns = " + millis + " ms )");
-		System.out.println(Arrays.deepToString(terms.toArray()));
+		System.out.println("Analysé en " + millis + " millisecondes");
+		System.out.println("Résultat de l'analyse : " + Arrays.deepToString(termes.toArray()));
 	}
 
 }
--- src/fr/calculator/resolution/TestSimplifieur.java
+++ src/fr/calculator/resolution/TestSimplifieur.java
@@ -1,69 +1,31 @@
 package fr.calculator.resolution;
 
 import fr.calculator.analyse.*;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class TestSimplifieur {
 
 	public static void main(String[] args) throws InterruptedException {
-		testerTermes();
-		System.out.println("--------------------------------------------");
-		testerExpression();
-		System.out.println("--------------------------------------------");
-		testerExpressionAvecFonctions();
-	}
-
-	private static void testerExpressionAvecFonctions() {
-		List<Terme> termes = new ArrayList<>();
-		termes.add(new Variable("x"));
-		termes.add(new Fonction(Fonction.NomFonction.COSINUS, new Variable("x")));
-		termes.add(new Fonction(Fonction.NomFonction.EXPONENTIELLE, new Parenthese(new Rationnel(3), new Rationnel(6, 2))));
-		termes.add(new Division(new Variable("x"), new Rationnel(-10)));
-		termes.add(new Puissance(new Variable("x"), new Rationnel(2)));
-		ExpressionSimple expression = MathSimplifieur.simplifierExpression(termes);
-		System.out.println("expression simplifiée : " + expression);
-	}
-
-	/**
-	 * Teste la fonction {@link MathSimplifieur#simplifierExpression(java.util.List)}.
-	 */
-	private static void testerExpression() {
-		Rationnel i = new Rationnel(5);
-		Multiplication m = new Multiplication(new Variable("x"), new Division(i, new Puissance(i, new Rationnel(2))));
-
-		List<Terme> termes = Arrays.asList(new Rationnel(-20), new Parenthese(m),
-				new Parenthese(new Rationnel(2), new Rationnel(25, 10)));
-
-		System.out.println("tel quel  : " + Arrays.deepToString(termes.toArray()));
-
+		System.out.println("Entrez une expression mathématique, ou appuyez sur entrée pour utiliser l'expression de test par défaut.");
+		Scanner sc = new Scanner(System.in);
+		String expression = sc.nextLine();
+		sc.close();
+		if (expression.isEmpty()) {
+			expression = "-22x + -24x^2 - +45 / 2 * (56+4) - cos(2) + 2 / 2";
+			System.out.println("L'expression par défaut va être utilisée : " + expression);
+		}
 		long t0 = System.nanoTime();
-		ExpressionSimple expression = MathSimplifieur.simplifierExpression(termes);
-		double time = (System.nanoTime() - t0) / (1000_000.0);
-
-		System.out.println("expression simplifiée : " + expression);
-		System.out.println("durée : " + time + " millisecondes");
-	}
-
-	/**
-	 * Teste la fonction {@link MathSimplifieur#simplifierTermes(java.util.List)}.
-	 */
-	private static void testerTermes() {
-		Rationnel i = new Rationnel(5);
-		Multiplication m = new Multiplication(new Variable("x"), new Division(i, new Puissance(i, new Rationnel(2))));
-
-		List<Terme> termes = Arrays.asList(new Rationnel(-20), new Parenthese(m),
-				new Parenthese(new Rationnel(2), new Rationnel(25, 10)));
-
-		System.out.println("tel quel  : " + Arrays.deepToString(termes.toArray()));
-
-		long t0 = System.nanoTime();
-		List<Terme> termesSimplifiés = MathSimplifieur.simplifierTermes(termes);
-		double time = (System.nanoTime() - t0) / (1000_000.0);
-
-		System.out.println("termes simplifiés : " + Arrays.deepToString(termesSimplifiés.toArray()));
-		System.out.println("durée : " + time + " millisecondes");
+		List<Terme> termes = new MathAnalyseur(expression).analyser();
+		long t1 = System.nanoTime();
+		ExpressionSimple expressionSimple = MathSimplifieur.simplifierExpression(termes);
+		long t2 = System.nanoTime();
+		double millisAnalyse = (t1 - t0) / Math.pow(10, 6);
+		double millisSimplification = (t2 - t1) / Math.pow(10, 6);
+		System.out.println("Analysé en " + millisAnalyse + " millisecondes");
+		System.out.println("Smplifié en " + millisSimplification + " millisecondes");
+		System.out.println("Expression simple : " + expressionSimple);
+		System.out.println("Termes simplifiés : " + expressionSimple.getTermes());
 	}
 
 }
